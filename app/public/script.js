1f0e5dd2b07b1aa0bf8100c36a2090c052c8bfcb
Christos Palmos <adm-christos.palmos@pon.com>
2023-10-25 10:31:06 +0200

    Added selectable names

--- app/public/script.js
+++ app/public/script.js
@@ -2,13 +2,12 @@ const nameElements = document.querySelectorAll(".name");
 const spinButton = document.getElementById("spin-button");
 const winnerName = document.getElementById("winner-name");
 const resultContainer = document.querySelector(".result-container");
-const spinSound = document.getElementById("spinSound"); // Get the audio element
+const spinSound = document.getElementById("spinSound");
 
 const names = [
   "Christos",
   "Huub",
   "Torben",
-  "Steven",
   "Stepan",
   "Osman",
   "Victorine",
@@ -18,12 +17,45 @@ const names = [
 ];
 
 let spinning = false;
+let selectedNames = [...names]; // Track selected names and initialize with all names
 
 function getRandomName() {
-  return names[Math.floor(Math.random() * names.length)];
+  return selectedNames[Math.floor(Math.random() * selectedNames.length)];
 }
 
 function initializeNames() {
+  nameElements.forEach((element) => {
+    element.textContent = getRandomName();
+  });
+
+  const namesList = document.getElementById("names-list");
+
+  namesList.innerHTML = ""; // Clear the names list
+
+  selectedNames.forEach((name) => {
+    const label = document.createElement("label");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = true; // By default, all names are selected
+    checkbox.value = name;
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        selectedNames.push(name);
+      } else {
+        selectedNames = selectedNames.filter(
+          (selectedName) => selectedName !== name
+        );
+      }
+      regenerateSlotMachineNames(); // Refresh slot machine names
+    });
+
+    label.appendChild(checkbox);
+    label.appendChild(document.createTextNode(name));
+    namesList.appendChild(label);
+  });
+}
+
+function regenerateSlotMachineNames() {
   nameElements.forEach((element) => {
     element.textContent = getRandomName();
   });
@@ -39,6 +71,11 @@ function rotateNames() {
 
   let currentFrame = 0;
   let spinnerResults = [];
+
+  // Get the initial names displayed in the slot-machine reels
+  nameElements.forEach((element) => {
+    spinnerResults.push(element.textContent);
+  });
 
   const spinInterval = setInterval(() => {
     currentFrame++;
@@ -62,14 +99,14 @@ function rotateNames() {
       let maxCount = 0;
 
       for (const name in nameCounts) {
-        if (nameCounts[name] >= maxCount) {
+        if (nameCounts[name] > maxCount) {
           mostCommonName = name;
           maxCount = nameCounts[name];
         }
       }
 
       if (maxCount === 1) {
-        // If all names are different, pick one at random
+        // If all names are different, pick one at random from the spinnerResults
         mostCommonName =
           spinnerResults[Math.floor(Math.random() * spinnerResults.length)];
       }
@@ -80,9 +117,9 @@ function rotateNames() {
       resultContainer.style.display = "block";
     } else {
       nameElements.forEach((element, index) => {
-        const randomIndex = Math.floor(Math.random() * names.length);
-        element.textContent = names[randomIndex];
-        spinnerResults[index] = names[randomIndex];
+        const randomIndex = Math.floor(Math.random() * selectedNames.length);
+        element.textContent = selectedNames[randomIndex];
+        spinnerResults[index] = selectedNames[randomIndex];
       });
     }
   }, interval);
